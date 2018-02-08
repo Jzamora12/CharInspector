bc212ec6746a31351cbec8ede65c3af0d41b7c33
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-08 11:39:17 -0800

    added another if statement and got it to output the ASCII value from a character.

--- main.cpp
+++ main.cpp
@@ -11,20 +11,25 @@ int main()
   //when user's entry is between A-Z...
   if ( Character>=65, Character<=90 )
   {
+  cout << "" << Character;
   cout<<" is an upper case letter!\n";
   }
   //when user's entry is between a-z...
   else if ( Character>=97, Character<=122) 
   {
+  cout << "" << Character;
   cout<<" is a lower case letter!\n";
   }
   //in all other cases...
   else 
   {
+  cout << "" << Character;
   cout<<"?! Pssh. What are you talking about?\n";
   }
   //no matter what they enter...
-  cout<<"The ASCII value is: " << Character; 
-
+  if ( Character>=65, Character<=122 )
+  {
+  cout<<"The ASCII value is: " << int(Character) ; 
+  }
   return 0;
 }
